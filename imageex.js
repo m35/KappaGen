05c4a31279d10cde08457dd54f0d24c3a4f52ab2
CBenni <levelcarnage@gmail.com>
2016-05-26 14:18:09 +0200

    Removed endsWith polyfill, its no longer needed

--- imageex.js
+++ imageex.js
@@ -1,14 +1,3 @@
-if (!String.prototype.endsWith) {
-	String.prototype.endsWith = function(searchString, position) {
-		var subjectString = this.toString();
-		if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
-		position = subjectString.length;
-		}
-		position -= searchString.length;
-		var lastIndex = subjectString.indexOf(searchString, position);
-		return lastIndex !== -1 && lastIndex === position;
-	};
-}
 var _imageCache = {};
 var ImageEx = function(src, isAnim) {
 	var self = this;
